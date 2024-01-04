92e7c96be2e9fb40d886a729fe51ad23c7717133
Bhavesh Jindal <72211665+Lucifer-bit0@users.noreply.github.com>
2024-01-04 22:03:22 +0530

    Update basic calci.py

--- basic calci.py
+++ basic calci.py
@@ -1,44 +1,59 @@
-def input_num():
-    a = int(input("Enter 1st Number: "))
-    b = int(input("Enter 2nd Number: "))
-    return a,b
-def addition(a,b):
-    print(f"Result: {a+b}")
-def subtraction(a,b):
-    print(f"Result: {a-b}")
-def multiplication(a,b):
-    print(f"Result: {a*b}")
-def division(a,b):
-    if b!=0:
-        print(f"Result: {a/b}")
-    else:
-        print("Dominator can't be Zero")
+from tkinter import *
 
-def welcome():
-
-    print(f"""Enter the arithmetic operator you want to use.
-        1--> ADDITION
-        2--> SUBTRACTION
-        3--> MULTIPLICATION
-        4--> DIVISION
-        5--> exit""")
-
-while(True):
-    welcome()
-    choice = int(input("Enter you choice number: "))
-    if choice == 1:
-        a,b = input_num()
-        addition(a,b)
-    elif choice==2:
-        a, b = input_num()
-        subtraction(a,b)
-    elif choice ==3:
-        a, b = input_num()
-        multiplication(a,b)
-    elif choice ==4:
-        a, b = input_num()
-        division(a,b)
-    elif choice ==5:
-        exit()
-    else :
-        print("Enter Valid Choice")
+def btnClick(number):
+    global val
+    val=val+str(number)
+    data.set(val)
+    
+def btnClear():
+    global val
+    val=""
+    data.set("")
+    
+def btnEquals():
+    global val
+    try:
+        result=str(eval(val))
+        data.set(result)
+    except:
+        data.set("Error")
+root=Tk()
+root.title("My Calculator")
+root.geometry('361x381+500+200')
+val=" "
+data=StringVar()
+display=Entry(root,textvariable=data,bd=29,justify="right",bg="powder blue", font=("Arial",20))
+display.grid(row=0,columnspan=4)
+btn7=Button(root,text="7",font=("Ariel",12,"bold"),bd=12,height=2,width=6,command=lambda:btnClick(7))
+btn7.grid(row=1,column=0)
+btn8=Button(root,text="8",font=("Ariel",12,"bold"),bd=12,height=2,width=6,command=lambda:btnClick(8))
+btn8.grid(row=1,column=1)
+btn9=Button(root,text="9",font=("Ariel",12,"bold"),bd=12,height=2,width=6,command=lambda:btnClick(9))
+btn9.grid(row=1,column=2)
+btn_add=Button(root,text="+",font=("Ariel",12,"bold"),bd=12,height=2,width=6,command=lambda:btnClick('+'))
+btn_add.grid(row=1,column=3)
+btn4=Button(root,text="4",font=("Ariel",12,"bold"),bd=12,height=2,width=6,command=lambda:btnClick(4))
+btn4.grid(row=2,column=0)
+btn5=Button(root,text="5",font=("Ariel",12,"bold"),bd=12,height=2,width=6,command=lambda:btnClick(5))
+btn5.grid(row=2,column=1)
+btn6=Button(root,text="6",font=("Ariel",12,"bold"),bd=12,height=2,width=6,command=lambda:btnClick(6))
+btn6.grid(row=2,column=2)
+btn_substract=Button(root,text="-",font=("Ariel",12,"bold"),bd=12,height=2,width=6,command=lambda:btnClick('-'))
+btn_substract.grid(row=2,column=3)
+btn1=Button(root,text="1",font=("Ariel",12,"bold"),bd=12,height=2,width=6,command=lambda:btnClick(1))
+btn1.grid(row=3,column=0)
+btn2=Button(root,text="2",font=("Ariel",12,"bold"),bd=12,height=2,width=6,command=lambda:btnClick(2))
+btn2.grid(row=3,column=1)
+btn3=Button(root,text="3",font=("Ariel",12,"bold"),bd=12,height=2,width=6,command=lambda:btnClick(3))
+btn3.grid(row=3,column=2)
+btn_divide=Button(root,text="/",font=("Ariel",12,"bold"),bd=12,height=2,width=6,command=lambda:btnClick('/'))
+btn_divide.grid(row=3,column=3)
+btnC=Button(root,text="C",font=("Ariel",12,"bold"),bd=12,height=2,width=6,command=btnClear)
+btnC.grid(row=4,column=0)
+btn0=Button(root,text="0",font=("Ariel",12,"bold"),bd=12,height=2,width=6,command=lambda:btnClick(0))
+btn0.grid(row=4,column=1)
+btnEqual=Button(root,text="=",font=("Ariel",12,"bold"),bd=12,height=2,width=6,command=btnEquals)
+btnEqual.grid(row=4,column=2)
+btn_multiply=Button(root,text="*",font=("Ariel",12,"bold"),bd=12,height=2,width=6,command=lambda:btnClick('*'))
+btn_multiply.grid(row=4,column=3)
+root.mainloop()
